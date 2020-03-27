e6e8bfeffb2e1a31e64e98754c3c362315c46bdc
crowdsalat <janweyrich93@gmail.com>
2020-03-27 12:55:32 +0100

    initial

---
+++ src/main/java/de/weyrich/kvbrad/KvbRadApplication.java
@@ -0,0 +1,13 @@
+package de.weyrich.kvbrad;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class KvbRadApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(KvbRadApplication.class, args);
+    }
+
+}
---
+++ src/test/java/de/weyrich/kvbrad/KvbRadApplicationTests.java
@@ -0,0 +1,13 @@
+package de.weyrich.kvbrad;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class KvbRadApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
