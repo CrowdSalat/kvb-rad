2b7467a889ae39f2d1628e3df302da99daa10e9a
crowdsalat <janweyrich93@gmail.com>
2020-03-28 10:20:15 +0100

    remove CommandLineRunner

--- src/main/java/de/weyrich/kvbrad/KvbRadApplication.java
+++ src/main/java/de/weyrich/kvbrad/KvbRadApplication.java
@@ -1,34 +1,22 @@
 package de.weyrich.kvbrad;
 
-import de.weyrich.kvbrad.model.nextbike.RootModel;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
-import java.net.URI;
-
 @SpringBootApplication
+@EnableScheduling
 public class KvbRadApplication {
+
+    @Bean
+    public RestTemplate restTemplate(){
+        return new RestTemplate();
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(KvbRadApplication.class, args);
     }
 
-    @Bean
-    public RestTemplate restTemplate(RestTemplateBuilder builder) {
-        return builder.build();
-    }
-
-    @Bean
-    public CommandLineRunner run(RestTemplate restTemplate) {
-        return args -> {
-            String url = "https://api.nextbike.net/maps/nextbike-official.json?city=14";
-            RootModel model = restTemplate.getForObject(url, RootModel.class);
-            System.out.println(model);
-        };
-    }
 }
