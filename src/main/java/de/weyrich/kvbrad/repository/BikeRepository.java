e03b315e63a200269225eb04c41e832eeebd52d6
Jan Weyrich <janweyrich93@gmail.com>
2020-04-09 08:58:15 +0200

    add two new query methods (property expressions)

--- src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
+++ src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
@@ -3,6 +3,13 @@ package de.weyrich.kvbrad.repository;
 import de.weyrich.kvbrad.model.jpa.Bike;
 import org.springframework.data.repository.CrudRepository;
 
+import java.util.List;
+import java.util.Optional;
+
 public interface BikeRepository extends CrudRepository<Bike, String> {
+
+    List<Bike> findByBikeIdOrderByCreationDateDesc(String bikeId);
+
+    Optional<Bike> findTopByBikeIdOrderByCreationDateDesc(String bikeId);
 
 }
