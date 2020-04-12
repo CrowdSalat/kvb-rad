a786349eb12edf010201aebe971a9a1ea7d20598
Jan Weyrich <janweyrich93@gmail.com>
2020-04-12 14:24:27 +0200

    schedule method every 5 minutes

--- src/main/java/de/weyrich/kvbrad/controller/ScheduledTasks.java
+++ src/main/java/de/weyrich/kvbrad/controller/ScheduledTasks.java
@@ -1,15 +1,12 @@
 package de.weyrich.kvbrad.controller;
 
 import de.weyrich.kvbrad.model.nextbike.RootModel;
-import de.weyrich.kvbrad.repository.BikeRepository;
 import de.weyrich.kvbrad.service.BikeHandlerService;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.client.RestTemplate;
 
 @Controller
 public class ScheduledTasks {
@@ -22,9 +19,10 @@ public class ScheduledTasks {
         this.bikeHandlerService = bikeHandlerService;
     }
 
-    @Scheduled(cron = "* * * * * *")
+
+    @Scheduled(fixedDelay = 300000)
     public void scheduleDownloadBikeData() {
-        logger.debug("download");
+        logger.info("download");
         RootModel rootModel = bikeHandlerService.downloadBikeDate();
         bikeHandlerService.saveToDatabase(rootModel);
     }
