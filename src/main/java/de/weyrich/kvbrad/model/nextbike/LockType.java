d1de8969328435832ac14dc7dc078f9213ef1a83
Jan Weyrich <janweyrich93@gmail.com>
2020-05-12 13:08:42 +0200

    add new locktype

--- src/main/java/de/weyrich/kvbrad/model/nextbike/LockType.java
+++ src/main/java/de/weyrich/kvbrad/model/nextbike/LockType.java
@@ -6,7 +6,7 @@ import com.fasterxml.jackson.annotation.JsonValue;
 import java.io.IOException;
 
 public enum LockType {
-    FORK_LOCK, ANALOG_CODE_LOCK, SCOOTER_MOTOR_LOCK ,UNDEFINED;
+    FORK_LOCK, ANALOG_CODE_LOCK, SCOOTER_MOTOR_LOCK, FRAME_LOCK ,UNDEFINED;
 
     @JsonValue
     public String toValue() {
@@ -24,6 +24,7 @@ public enum LockType {
         if (value.equals("fork_lock")) return FORK_LOCK;
         if (value.equals("analog_code_lock")) return ANALOG_CODE_LOCK;
         if (value.equals("scooter_motor_lock")) return SCOOTER_MOTOR_LOCK;
+        if (value.equals("frame_lock")) return FRAME_LOCK;
         if (value.equals("")) return UNDEFINED;
         throw new IOException("Cannot deserialize LockType with value " + value);
     }
