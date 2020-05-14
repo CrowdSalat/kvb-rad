e34a1f88d417d5c2ec716cfeea568264b892ef2f
Jan Weyrich <janweyrich93@gmail.com>
2020-05-14 11:16:01 +0200

    test for BikeMovementService

--- src/test/java/de/weyrich/kvbrad/TestBikeMovementService.java
+++ src/test/java/de/weyrich/kvbrad/TestBikeMovementService.java
@@ -1,54 +1,65 @@
 package de.weyrich.kvbrad;
 
-import static org.hamcrest.MatcherAssert.assertThat;
-import static org.hamcrest.Matchers.*;
-
+import de.weyrich.kvbrad.model.graphhopper.Path;
+import de.weyrich.kvbrad.model.graphhopper.Welcome;
 import de.weyrich.kvbrad.model.jpa.Bike;
 import de.weyrich.kvbrad.model.jpa.Tour;
-import de.weyrich.kvbrad.repository.BikeRepository;
 import de.weyrich.kvbrad.repository.TourRepository;
 import de.weyrich.kvbrad.service.BikeMovementService;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.web.client.MockRestServiceServer;
+import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.client.RestTemplate;
 
-@SpringBootTest
+import static org.hamcrest.MatcherAssert.assertThat;
+import static org.hamcrest.Matchers.*;
+import static org.mockito.Mockito.any;
+import static org.mockito.Mockito.*;
+
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.NONE)
 public class TestBikeMovementService {
-
-    public final double LOC_HEUMARKT_LAT = 50.936256;
-    public final double LOC_HEUMARKT_LNG = 6.960639;
-
-    public final double LOC_FREIHEIT_LAT = 50.936838;
-    public final double LOC_FREIHEIT_LNG = 6.971713;
-
-    private BikeMovementService bikeMovementService;
 
     @Autowired
     private TourRepository tourRepo;
-    @Autowired
-    private BikeRepository bikeRepo;
-    @Autowired
+
+    @MockBean
     private RestTemplate restTemplate;
 
-    private MockRestServiceServer graphhopperMockServer;
+    @Autowired
+    private BikeMovementService bikeMovementService;
 
-    private final String graphhopperMockUrl = "http://localhost:12345/route";
-
+    private Welcome grahphopperResponse = createGraphhopperResponse();
 
     @BeforeEach
     public void setUp() {
         tourRepo.deleteAll();
-        bikeMovementService = new BikeMovementService(restTemplate, bikeRepo, tourRepo,graphhopperMockUrl);
-        graphhopperMockServer.createServer(restTemplate);
+
     }
 
+    @Test
+    public void handleMovement_fromTwoDifferentBikes_AbortCalc() {
+        // GIVEN
+        Bike bike = new Bike("123", 1.0, 1.0);
+        Bike bikeOld = new Bike("234", 2.0, 2.0);
+
+        // WHEN
+        bikeMovementService.handleMovement(bike, bikeOld);
+
+        // THEN
+        final Iterable<Tour> all = tourRepo.findAll();
+        assertThat(all, iterableWithSize(0));
+    }
 
     @Test
     public void handleMovement_witValidData() {
         // GIVEN
+        when(restTemplate.getForEntity(anyString(), eq(Welcome.class), any(), any(), any(), any()))
+                .thenReturn(new ResponseEntity(this.grahphopperResponse, HttpStatus.OK));
+
         Bike bike = new Bike("123", 1.0, 1.0);
         Bike bikeOld = new Bike("123", 2.0, 2.0);
 
@@ -69,6 +80,8 @@ public class TestBikeMovementService {
     @Test
     public void handleMovement_withoutActualMovement() {
         // GIVEN
+        when(restTemplate.getForEntity(anyString(), eq(Welcome.class), any(), any(), any(), any()))
+                .thenReturn(new ResponseEntity(this.grahphopperResponse, HttpStatus.OK));
         Bike bike = new Bike("234", 1.0, 1.0);
         Bike bikeOld = new Bike("234", 1.0, 1.0);
 
@@ -82,7 +95,17 @@ public class TestBikeMovementService {
 
 
     @Test
-    public void handleMovement_withAPIError(){
+    public void handleMovement_withAPIError() {
 
     }
+
+
+    private Welcome createGraphhopperResponse() {
+        Welcome grahphopperResponse = new Welcome();
+        Path path = new Path();
+        path.setDistance(999.0);
+        grahphopperResponse.setPaths(new Path[]{path});
+        return grahphopperResponse;
+    }
+
 }
