5007c9ec4152ddccb6c79443b26977653aec0046
Jan Weyrich <janweyrich93@gmail.com>
2020-05-14 11:29:41 +0200

    activate async support

--- src/main/java/de/weyrich/kvbrad/KvbRadApplication.java
+++ src/main/java/de/weyrich/kvbrad/KvbRadApplication.java
@@ -6,12 +6,14 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
 import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
+import org.springframework.scheduling.annotation.EnableAsync;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 @EnableScheduling
 @EnableJpaAuditing
+@EnableAsync
 public class KvbRadApplication {
 
     private static final Logger logger = LoggerFactory.getLogger(KvbRadApplication.class);
