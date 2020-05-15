74571b386a6f7e6f55b8efba0962b57e0f27a557
Jan Weyrich <janweyrich93@gmail.com>
2020-05-15 15:31:18 +0200

    do not export create and delete over rest

--- src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
+++ src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
@@ -1,7 +1,9 @@
 package de.weyrich.kvbrad.repository;
 
 import de.weyrich.kvbrad.model.jpa.Bike;
+import de.weyrich.kvbrad.model.jpa.Tour;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.rest.core.annotation.RestResource;
 
 import java.util.List;
 import java.util.Optional;
@@ -12,4 +14,28 @@ public interface BikeRepository extends CrudRepository<Bike, String> {
 
     Optional<Bike> findTopByBikeIdOrderByCreationDateDesc(String bikeId);
 
+    // do not export create and delete over rest
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Bike var1);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Bike> var1);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Bike> S save(S var1);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Bike> Iterable<S> saveAll(Iterable<S> var1);
 }
--- src/main/java/de/weyrich/kvbrad/repository/TourRepository.java
+++ src/main/java/de/weyrich/kvbrad/repository/TourRepository.java
@@ -4,6 +4,7 @@ import de.weyrich.kvbrad.model.jpa.Tour;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
+import org.springframework.data.rest.core.annotation.RestResource;
 import org.springframework.format.annotation.DateTimeFormat;
 import org.springframework.web.bind.annotation.RequestParam;
 
@@ -19,5 +20,30 @@ public interface TourRepository extends CrudRepository<Tour, String> {
     List<Tour> findToursBetweenDates(
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam("start") @Param("start") Date startDate,
             @DateTimeFormat(iso = DateTimeFormat.ISO.DATE) @RequestParam("end") @Param("end") Date endDate);
+
+    // do not export create and delete over rest
+    @Override
+    @RestResource(exported = false)
+    void deleteById(String aLong);
+
+    @Override
+    @RestResource(exported = false)
+    void delete(Tour var1);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll(Iterable<? extends Tour> var1);
+
+    @Override
+    @RestResource(exported = false)
+    void deleteAll();
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> S save(S var1);
+
+    @Override
+    @RestResource(exported = false)
+    <S extends Tour> Iterable<S> saveAll(Iterable<S> var1);
 
 }
