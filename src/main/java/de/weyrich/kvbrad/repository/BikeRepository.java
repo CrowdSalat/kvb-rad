43b9bd13073f7f67faeb05b3f27b7e17edd95f7a
Jan Weyrich <janweyrich93@gmail.com>
2020-07-01 11:30:12 +0200

    do not export BikeRepository via spring data rest

--- src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
+++ src/main/java/de/weyrich/kvbrad/repository/BikeRepository.java
@@ -8,34 +8,11 @@ import org.springframework.data.rest.core.annotation.RestResource;
 import java.util.List;
 import java.util.Optional;
 
+@RestResource(exported = false)
 public interface BikeRepository extends CrudRepository<Bike, String> {
 
     List<Bike> findByBikeIdOrderByCreationDateDesc(String bikeId);
 
     Optional<Bike> findTopByBikeIdOrderByCreationDateDesc(String bikeId);
 
-    // do not export create and delete over rest
-    @Override
-    @RestResource(exported = false)
-    void deleteById(String aLong);
-
-    @Override
-    @RestResource(exported = false)
-    void delete(Bike var1);
-
-    @Override
-    @RestResource(exported = false)
-    void deleteAll(Iterable<? extends Bike> var1);
-
-    @Override
-    @RestResource(exported = false)
-    void deleteAll();
-
-    @Override
-    @RestResource(exported = false)
-    <S extends Bike> S save(S var1);
-
-    @Override
-    @RestResource(exported = false)
-    <S extends Bike> Iterable<S> saveAll(Iterable<S> var1);
 }
